b68fca82af9b0d0caa2fff88e917c40f4e7043d8
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-23 15:49:55 -0700

    Add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+import { ADD } from '../actions';
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+export default messageReducer;
